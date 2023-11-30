997502f29f0e9de900c30aabbd866e7a073412c7
Aish Mishra <aishlunatic001@gmail.com>
2023-11-30 12:09:08 +0530

    Updated code

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -14,6 +14,24 @@ updateScoreElement();
  }
  */
 // !score is same as score === null 
+let isAutoPlaying = false;
+let intervalId ; 
+function autoPlay(){
+  
+  if(!isAutoPlaying){
+   intervalId =  setInterval(function(){
+      const playerMove = pickComputerMove();
+      playGame(playerMove);
+    },1000);
+    isAutoPlaying = true ; 
+  }
+  else{
+     clearInterval(intervalId);
+     isAutoPlaying = false ; 
+  }
+
+  
+}
 
 
 function playGame(playerMove) {
@@ -55,7 +73,7 @@ function playGame(playerMove) {
     score.losses += 1;
   }
   else if (result === 'Tie.') {
-    sroce.ties += 1;
+    score.ties += 1;
   }
   localStorage.setItem('score', JSON.stringify(score));
 
